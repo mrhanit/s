16aecbbb29f06ae425e6a47f4997e8ebbd816155
HT <HT@HanSiro>
2014-11-18 13:41:25 +0700

    dssf

--- src/dfa/a.java
+++ src/dfa/a.java
@@ -2,4 +2,13 @@ package dfa;
 
 public class a {
 
+	public static void main(String[] args) {
+		for (int i = 0; i < args.length; i++) {
+			for (int j = 0; j < args.length; j++) {
+				for (int j2 = 0; j2 < args.length; j2++) {
+					
+				}
+			}
+		}
+	}
 }
